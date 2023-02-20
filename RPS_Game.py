ec9dd8b7b3f2f4e2f31ce0c9c16b83087a4ffd13
Oscar Villalobos <oscarjvillalobosc@gmail.com>
2023-02-20 14:53:46 -0500

    Added a linked list that shows what each player chose

--- RPS_Game.py
+++ RPS_Game.py
@@ -1,16 +1,42 @@
 
+class Node:
+    def __init__(self, player, choice):
+        self.player = player
+        self.choice = choice
+        self.next = None
+
+
 # Defining the player through a class
 class Player:
     def __init__(self, name):
         self.name = name
         self.score = 0
-        self.choice = None
+        self.history = None
+        self.tail = None
+# method that describes the history of a player's moves
 
-# function that describes the choice
+    def add_move_to_history(self, choice):
+        if self.history is None:
+            self.history = Node(self, choice)
+            self.tail = self.history
+        else:
+            new_node = Node(self, choice)
+            self.tail.next = new_node
+            self.tail = new_node
+
+# method that describes the choice
     def choose(self):
-        self.choice = input(f"{self.name}, please choose (rock, paper, or scissors): ").lower()
-        while self.choice not in ["rock", "paper", "scissors"]:
-            self.choice = input("Invalid choice. Please try again").lower()
+        choice = input(f"{self.name}, please choose (rock, paper, or scissors): ").lower()
+        while choice not in ["rock", "paper", "scissors"]:
+            choice = input("Invalid choice. Please try again").lower()
+        self.add_move_to_history(choice)
+        self.choice = choice
+# method that describes the player's move history
+    def print_history(self):
+        current_node = self.history
+        while current_node is not None:
+            print(f"{self.name} chose {current_node.choice}")
+            current_node = current_node.next
 
 # Defining the Game through a class
 class Game:
@@ -62,10 +88,12 @@ class Game:
 player1 = Player("Player 1")
 player2 = Player("Player 2")
 
+
 # Game invoked by an object
 game = Game(player1,player2)
 
 # Invoking the game by calling the function
 game.play_game()
 
-
+# Print out the move history for each player
+player1.print_history
